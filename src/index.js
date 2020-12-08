81fd3aea821a80fbedfa4f9f26413925389cd153
Oleksandr Slipchenko <o.p.slipchenko@gmail.com>
2020-12-09 00:48:25 +0200

    Начальная разметка

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+<<<<<<< Updated upstream
 import './css/normalize.css';
 import './css/reset.css';
 import './css/main.min.css';
+=======
+// import './styles.css';
+import './sass/main.scss';
+>>>>>>> Stashed changes
